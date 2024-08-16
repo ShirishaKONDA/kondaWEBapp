4569f89da6e79ae59a817f8979aac06eccf22e0e
ShirishaKONDA <shirisha.k1122@gmail.com>
2024-08-15 22:28:01 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "./components/layout/layout";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about'
+import Menu from "./pages/menu";
+import Contact from './pages/contact'
+import PageNotFound from './pages/pagenotfound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
